1051cc1b79da1ea7178611d5c47387d9922bcdf6
rodriguezcravero <nacho.rodriguez85@gmail.com>
2020-03-04 11:49:17 -0300

    cambios realizados en estilo y funcionalidad

--- app.js
+++ app.js
@@ -1,80 +1,80 @@
 
+/*++++++++++++++DECLARACION DE VARIABLES++++++++++++++*/
 let palabra1 = document.querySelector('.palabra1');
 let palabra2 = document.querySelector('.palabra2');
 const boton = document.querySelector('#task');
-
 const mensaje = document.querySelector('.msj');
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++*/
 
-let contador1 = 0;
-let contador2 = 0;
-let letrasiguales = 0;
 
+/*+++++++++++++FUNCIONALIDAD DEL PROGRAMA+++++++++++++*/
+
+//Cada submit del botón 'verificar' inicia la función principal.
 boton.addEventListener('submit', iniciar);
 
-
-function iniciar(e) {
-
-e.preventDefault();
-
-let str1 = palabra1.value.toLowerCase();
-let str2 = palabra2.value.toLowerCase();
-
-let p1 = Array.from(str1);
-let p2 = Array.from(str2);
-
-console.log(p1);
-console.log(p2);
-
-if (str1.length == str2.length){
-p1.forEach(myFunction);
-
-	console.log("Letras iguales: " + letrasiguales);
-	if(letrasiguales == str1.length){
-		mensaje.innerHTML = "Es un anagrama!"
-		setTimeout(message, 2500);
-		palabra1.value = "";
-		palabra2.value = "";
-	}
-	else{
-		mensaje.innerHTML = "No es un anagrama";
-		setTimeout(message, 2500);
-		palabra1.value = "";
-		palabra2.value = "";
-	}
-
-letrasiguales = 0;
-
-}
-else
-
-mensaje.innerHTML = "Las palabras deben tener la misma cantidad de letras";
-
-
-function message(){
-	mensaje.innerHTML = "";
+//Función que borra del input las palabras ingresadas.
+function limpiar(){
+	palabra1.value = "";
+	palabra2.value = "";
 }
 
-function myFunction(x){
+//Función que después de 2,5 segundos borra el mensaje de resultados.
+function limpiarMensaje(){
+	mensaje.innerText = "";
+}
+
+//Función que chequea si dos palabras son o no un anagrama.
+//Recibe dos arrays de letras ordenados, y chequea con un for si los elementos en la misma posición son iguales.
+//Solamente es llamado cuando ambos arrays tienen la misma cantidad de elementos.
+function anagrama(p1, p2){
+	let flag = true;
 	for (let i = 0; i < p1.length; i++) {
-
-		if(x === p1[i]){
-			contador1++;
+		if(p1[i] !== p2[i]){
+			flag = false;
+			break;
 		}
 	}
-	
-	for (let j = 0; j < p1.length; j++) {
+	return flag;
+}
 
-		if(x === p2[j]){
-			contador2++;
+//Función principal.
+function iniciar(e){
+
+	//Como hay un 'submit', se detiene el reload de la página.
+	e.preventDefault();
+
+	//Guardo en 2 arreglos ambas palabras, pasadas a minúsculas.
+	let p1 = Array.from(palabra1.value.toLowerCase());
+	let p2 = Array.from(palabra2.value.toLowerCase());
+
+	//Ordeno ambos arreglos. El sort sirve para chars, y si hay números en el array, serán tratados como chars.
+	p1.sort();
+	p2.sort();
+	
+	//Primero chequeo que se hayan ingresado dos palabras y no haya uno o dos campos vacíos.
+	if(p1.length == 0 || p2.length == 0){
+		mensaje.innerText = "Se deben ingresar dos palabras";
+		setTimeout(limpiarMensaje, 2500);
+	} else {
+		//Luego chequeo que tengan la misma cantidad de letras, sino se muestra el mensaje del 'else'.
+		if(p1.length === p2.length){
+			//La función 'anagrama()' retorna un boolean.
+			if(!anagrama(p1, p2)){
+				mensaje.innerText = `Las palabras '${palabra1.value}' y '${palabra2.value}' no resultan en un anagrama`;
+				//Se hace un 'timeout' para borrar el mensaje de resultado después de 2,5 segundos.
+				setTimeout(limpiarMensaje, 2500);
+			} else {
+				mensaje.innerText = `Las palabras '${palabra1.value}' y '${palabra2.value}' sí forman un anagrama`;
+				setTimeout(limpiarMensaje, 2500);
+			}
+		} else {
+			mensaje.innerText = "No es anagrama porque no tienen la misma cantidad de letras";
+			setTimeout(limpiarMensaje, 2500);
 		}
 	}
-	
-	if(contador1 === contador2)
-		letrasiguales++;
 
-	contador1 = 0;
-	contador2 = 0;
+	//Se llama a la función para limpiar los dos inputs.
+	limpiar();
 }
 
-}
-
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++*/
